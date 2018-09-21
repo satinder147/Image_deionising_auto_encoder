c0f7c981473e56159d346cf1d7377f51d327558f
satinder147 <satindersingh_16277@aitpune.edu.in>
2018-09-22 01:19:33 +0530

    Completed the project

--- model_trainer.py
+++ model_trainer.py
@@ -16,11 +16,11 @@ load_img=loader()
 mod=Models(w,h,c)
 auto_encoder=mod.Arch1()
 auto_encoder.summary()
-x_data,y_data=load_img.load('new')
+x_data,y_data=load_img.load('stone','paper','scissor')
 x_data=np.array(x_data,dtype='float')/255.0
 y_data=np.array(y_data,dtype='float')/255.0
 opt=Adam(lr=0.001,decay=0.001/50)
 train_x,test_x,train_y,test_y=train_test_split(x_data,y_data,test_size=0.1,random_state=30)
 auto_encoder.compile(optimizer='adadelta',loss='binary_crossentropy')
-auto_encoder.fit(train_x,train_y,batch_size=32,shuffle='true',epochs=50,validation_data=(test_x,test_y),verbose=1)
+auto_encoder.fit(train_x,train_y,batch_size=32,shuffle='true',epochs=15,validation_data=(test_x,test_y),verbose=1)
 auto_encoder.save('noise.MODEL')
--- runner.py
+++ runner.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 n=noise()
 m=Models(240,240,3)
-img=cv2.imread('a.jpg',1)
+img=cv2.imread('test/71.jpg',1)
 img=cv2.resize(img,(240,240))
 noise=n.add_noise(img)
 img2=noise
@@ -17,8 +17,9 @@ img2=img_to_array(img2)
 img2=np.expand_dims(img2,axis=0)
 print(img2.shape)
 mod=m.Arch1()
-mod.load_weights('auto.MODEL')
+mod.load_weights('noise.MODEL')
 print('hello')
 img=mod.predict(img2)[0]
 cv2.imshow('d',img)
+cv2.imshow('noise',noise)
 cv2.waitKey(0)
