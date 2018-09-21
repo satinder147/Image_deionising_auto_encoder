0fd7de0bfd74bc76ce41e5edd788c4fddc8f65dc
satinder147 <satindersingh_16277@aitpune.edu.in>
2018-09-21 10:51:02 +0530

    nn

--- model_trainer.py
+++ model_trainer.py
@@ -9,15 +9,18 @@ w=240
 h=240
 c=3
 
-load_img=loader()
+
+mod=Models(w,h,c)
+auto_encoder=mod.Arch1()
+'''load_img=loader()
 mod=Models(w,h,c)
 auto_encoder=mod.Arch1()
 auto_encoder.summary()
 x_data,y_data=load_img.load('new')
 x_data=np.array(x_data,dtype='float')/255.0
 y_data=np.array(y_data,dtype='float')/255.0
-opt=Adam(lr=0.01,decay=0.01/50)
+opt=Adam(lr=0.001,decay=0.001/50)
 train_x,test_x,train_y,test_y=train_test_split(x_data,y_data,test_size=0.1,random_state=30)
-auto_encoder.compile(optimizer=opt,loss='binary_crossentropy')
-auto_encoder.fit(train_x,train_y,batch_size=32,shuffle='true',epochs=50,validation_data=(test_x,test_y),verbose=1)
-auto_encoder.save('auto.MODEL')
+auto_encoder.compile(optimizer='adadelta',loss='binary_crossentropy')
+auto_encoder.fit(train_x,train_y,batch_size=32,shuffle='true',epochs=100,validation_data=(test_x,test_y),verbose=1)
+auto_encoder.save('auto.MODEL')'''
--- resizer.py
+++ resizer.py
@@ -1,13 +1,13 @@
 import cv2
 import os
 import numpy
-path='dataset'
-os.mkdir('new')
+path='data'
+os.mkdir('neww')
 paths=os.listdir(path)
 
 count=1
 for i in paths:
     img=cv2.imread(path+'/'+i,1)
     img=cv2.resize(img,(240,240))
-    cv2.imwrite('new/'+str(count)+'.jpg',img)
+    cv2.imwrite('neww/'+str(count)+'.jpg',img)
     count=count+1
